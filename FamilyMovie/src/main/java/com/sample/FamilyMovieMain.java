bf11e80f41c196b07000538c816b81d7913245dc
dj-chemik <dj-chemik@wp.pl>
2019-12-08 00:53:13 +0100

    Add confirmation by button with multi choice panel

--- FamilyMovie/src/main/java/com/sample/FamilyMovieMain.java
+++ FamilyMovie/src/main/java/com/sample/FamilyMovieMain.java
@@ -4,13 +4,16 @@ import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JOptionPane;
+import javax.xml.stream.events.StartDocument;
 
-public class FamilyMovieMain {
+public class FamilyMovieMain{
 
 	public static final void main(String[] args) {
         try {
@@ -32,21 +35,45 @@ public class FamilyMovieMain {
 		return JOptionPane.showOptionDialog(null, question, "It's a question to you", optionType, messageType[2], null, options, null);
 	}
 	
+	 
 	public static int multiplicityQuestion(String question, ArrayList<String> options){
 		ArrayList<Object> boxes = new ArrayList<>();
 		for (int i = 0; i < options.size(); i++) {
 			boxes.add(new JCheckBox(options.get(i)));
 		}
-		boxes.add(new JButton("Confirm"));
-		Object[] panel = boxes.toArray(); 
-		int result = JOptionPane.showOptionDialog(null, question, "It's a question to you", optionType, messageType[2], null, panel, null);
-		return result;
+		
+		ArrayList<Integer> results = new ArrayList<>();
+		JButton confirmButton = new JButton("Confirm");
+		boxes.add(confirmButton);
+		Object[] panel = boxes.toArray();
+		JOptionPane pane = new JOptionPane();
+		confirmButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				for (int i=0; i<boxes.size()-1; i++) {
+					if( 	((JCheckBox)boxes.get(i)).isSelected()) {
+						//return i;
+						results.add(i);			
+					}
+				}
+				pane.getRootFrame().dispose();								 
+			  }
+		});
+		
+				
+		pane.showOptionDialog(null, question, "It's a question to you", optionType, messageType[2], null, panel, null);
+		for (int i = 0; i < results.size(); i++) {
+			System.out.println(options.get(results.get(i)));
+		}
+		
+		return 0;
 	}
     
+	
 	public static void respondBoard(String respond) {
 		String ok[] = {"Ok"};
 		JOptionPane.showOptionDialog(null, respond, "Respond", optionType, messageType[1], null, ok, ok[0]);
 	}
-	  
+
+		  
 
 }
