92eb75a188b079ccadb0bf70735fb82e6c81e4a6
dj-chemik <dj-chemik@wp.pl>
2019-12-07 21:11:07 +0100

    Add Swing's GUI methods to main class

--- FamilyMovie/src/main/java/com/sample/FamilyMovieMain.java
+++ FamilyMovie/src/main/java/com/sample/FamilyMovieMain.java
@@ -17,7 +17,19 @@ public class FamilyMovieMain {
             t.printStackTrace();
         }
     }
+	
+	private static int optionType = JOptionPane.DEFAULT_OPTION;
+	private static int messageType[] = {JOptionPane.PLAIN_MESSAGE, JOptionPane.INFORMATION_MESSAGE, 
+										JOptionPane.QUESTION_MESSAGE, JOptionPane.WARNING_MESSAGE, JOptionPane.ERROR_MESSAGE};
+	
+	public static int questionBoard(String question, String[] answers) {
+		return JOptionPane.showOptionDialog(null, question, "It's a question to you", optionType, messageType[2], null, answers, null);
+	}
     
+	public static void respondBoard(String respond) {
+		String ok[] = {"Ok"};
+		JOptionPane.showOptionDialog(null, respond, "Respond", optionType, messageType[1], null, ok, ok[0]);
+	}
    
 
 }
